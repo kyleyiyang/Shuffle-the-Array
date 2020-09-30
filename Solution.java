f4576e62d45d9900e68b53e3a7921b4d1807060c
kyleyiyang <kyleyiyang@gmail.com>
2020-09-29 23:15:28 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -13,3 +13,26 @@ class Solution {
         
     }
 }
+//Another solution
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] l = new int[n];
+        int[] r = new int[n];
+        for (int i=0; i<n; i++) {
+            l[i] = nums[i];
+            r[i] = nums[i+n];
+        }
+        int k=0; int m=0;
+        for (int j=0; j<2*n; j++) {
+            if (j%2==0) {
+                nums[j] = l[k];
+                k = k +1;
+            } else {
+                nums[j] = r[m];
+                m = m+1;
+            }
+             
+        }
+        return nums;
+    }
+}
